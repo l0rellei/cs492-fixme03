82bdafa1a5bf4bdb7385dfed7c487dd9382bcb77
Florentina Ilie <ilief@oregonstate.edu>
2019-10-09 08:45:30 -0700

    Fixed call to super class in Main Activity

--- app/src/main/java/edu/osucascades/cs492/fixme3/MainActivity.java
+++ app/src/main/java/edu/osucascades/cs492/fixme3/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStart() {
-        sub.onStart();
+        super.onStart();
         Log.d(TAG, "onStart() called");
     }
 
